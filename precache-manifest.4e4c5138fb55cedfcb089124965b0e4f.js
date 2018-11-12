2a4c4689c4e59f8878bdea398ef8487c33faf447
khalilbenachir <benachir1996khalil@gmail.com>
2018-11-12 13:06:17 +0000

    Updates

---
+++ precache-manifest.4e4c5138fb55cedfcb089124965b0e4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/khalilbenachir/BurgerApp/static/media/burgerlogo.b8503d26.png"
+  },
+  {
+    "revision": "396d3a1495451ca9f304",
+    "url": "/khalilbenachir/BurgerApp/static/js/runtime~main.396d3a14.js"
+  },
+  {
+    "revision": "30b02151ebf3ab95f074",
+    "url": "/khalilbenachir/BurgerApp/static/js/main.30b02151.chunk.js"
+  },
+  {
+    "revision": "46780b80980aa0914501",
+    "url": "/khalilbenachir/BurgerApp/static/js/1.46780b80.chunk.js"
+  },
+  {
+    "revision": "30b02151ebf3ab95f074",
+    "url": "/khalilbenachir/BurgerApp/static/css/main.12e6ac6e.chunk.css"
+  },
+  {
+    "revision": "4ab20b5ea59c4397fcd3b97c60115626",
+    "url": "/khalilbenachir/BurgerApp/index.html"
+  }
+];
